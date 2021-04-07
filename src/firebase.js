a37461c8e1969f0dddaa772d74adc3b2e2cc7924
Praveen Teotia <37291981+Praveen-thedev@users.noreply.github.com>
2021-04-07 21:43:48 +0530

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
- const firebaseConfig = {
-    apiKey: "AIzaSyDXH5eG_0yGR5CTzvk6vsWLzdVSUhNOAX8",
-    authDomain: "clone-2e89c.firebaseapp.com",
-    projectId: "clone-2e89c",
-    storageBucket: "clone-2e89c.appspot.com",
-    messagingSenderId: "1095174593086",
-    appId: "1:1095174593086:web:5aba2f088d3a1f6f568f6f",
-    measurementId: "G-6JZ1MHS83X"
-  };
